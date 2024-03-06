e195b561e7b292d657d27c6c89cf2278dbb67a77
Tane4ka170 <tane4ka170@gmail.com>
2024-03-07 01:05:02 +0200

    fetch random meal

---
+++ script.js
@@ -0,0 +1,44 @@
+const meals = document.getElementById("meals");
+
+getRandomMeal();
+
+async function getRandomMeal() {
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+  const respData = await resp.json();
+  const randomMeal = respData.meals[0];
+
+  addMeal(randomMeal, true);
+}
+
+async function getMealById(id) {
+  const meal = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+  );
+}
+
+async function getMealBySearch(term) {
+  const meals = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=" + termasync
+  );
+}
+
+function addMeal(mealData, random = false) {
+  const meal = document.createElement("div");
+  meal.classList.add("meal");
+
+  meal.innerHTML = `<div class="meal-header">
+          ${random ? '<span class="random">Unpredictable dish</span>' : ""}
+          <img
+            src="${mealData.strMealThumb}"
+            alt="${mealData.strMeal}"
+          />
+        </div>
+        <div class="meal-body">
+          <h4>${mealData.strMeal}</h4>
+          <button class="fav-btn active"><i class="fas fa-heart"></i></button>
+        </div>`;
+
+  meals.appendChild(meal);
+}
